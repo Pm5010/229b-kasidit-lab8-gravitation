7369595797760380d6e5ac4658dfa2ce2ae4b72b
Pm5010 <kasidit.ngan@bumail.net>
2025-03-10 16:31:03 +0700

    end

--- Assets/Asteroids Pack/Gitsk/Grvit.cs
+++ Assets/Asteroids Pack/Gitsk/Grvit.cs
@@ -1,22 +1,43 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Grvit : MonoBehaviour
 {
     Rigidbody rb;
+    const float G = 0.006674f;
+    public static List<Grvit> planetLists;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        if (planetLists == null)
+        {
+            planetLists = new List<Grvit>();
+        }
+        
+        planetLists.Add(this);
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (var planet in planetLists)
+        {
+            if(planet != this)
+            attract(planet);
+        }
     }
 
     void attract(Grvit other)
     {
-        Rigidbody ohterRb = other.rb;
+        Rigidbody otherRb = other.rb;
 
-        Vector3 direction = rb.position - ohterRb.position;
+        Vector3 direction = rb.position - otherRb.position;
         float distance = direction.magnitude;
+
+        float forceMagnitude = G * ((rb.mass * otherRb.mass) / Mathf.Pow(distance, 2));
+        Vector3 finalForce = forceMagnitude * direction.normalized;
         
-        float forceMagnitude = 
+        otherRb.AddForce(finalForce);
     }
 }
